5408fd8addc99e56e3a401e0c6ba7ea9a94070fc
Hyune-c <choi8608@gmail.com>
2022-12-29 23:14:24 +0900

    ✨ barchart_material - 하드코딩으로 구현

--- src/main/kotlin/com/example/googlechartgenerator/BarchartMaterialRequest.kt
+++ src/main/kotlin/com/example/googlechartgenerator/BarchartMaterialRequest.kt
@@ -2,6 +2,12 @@ package com.example.googlechartgenerator
 
 data class BarchartMaterialRequest(
     val title: String,
-    val header: String,
-    val body: List<String>
+    val header: List<String>,
+    val body: List<Body>
+)
+
+data class Body(
+    val first: String,
+    val second: Long,
+    val third: Long
 )
--- src/main/kotlin/com/example/googlechartgenerator/ChartController.kt
+++ src/main/kotlin/com/example/googlechartgenerator/ChartController.kt
@@ -1,24 +1,38 @@
 package com.example.googlechartgenerator
 
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestBody
-
+import org.springframework.web.servlet.ModelAndView
 
 @Controller
 class ChartController {
     @GetMapping("/charts/barchart_material")
-    fun chart(@RequestBody requestString: String): String {
-        val convert = requestString.convert()
-        return "barchart_material"
+    fun chart(
+        @RequestBody requestString: String,
+        model: Model
+    ): ModelAndView {
+        val request = requestString.convert()
+
+        val mav = ModelAndView()
+        mav.addObject("request", request)
+        mav.viewName = "barchart_material"
+
+        return mav
     }
 }
 
 private fun String.convert(): BarchartMaterialRequest =
     split("\n").let {
+        val drop = it.drop(2)
+            .map { it2 ->
+                val split = it2.split(",")
+                Body(split[0], split[1].toLong(), split[2].toLong())
+            }
         BarchartMaterialRequest(
             title = it[0],
-            header = it[1],
-            body = it.drop(2)
+            header = it[1].split(","),
+            body = drop
         )
     }
