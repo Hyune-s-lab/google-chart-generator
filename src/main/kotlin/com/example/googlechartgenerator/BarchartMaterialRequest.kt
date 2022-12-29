576f7730637c82bae8b91974551373bbcde1f41c
Hyune-c <choi8608@gmail.com>
2022-12-29 22:25:56 +0900

    ✨ barchart_material - request 구현

---
+++ src/main/kotlin/com/example/googlechartgenerator/BarchartMaterialRequest.kt
@@ -0,0 +1,7 @@
+package com.example.googlechartgenerator
+
+data class BarchartMaterialRequest(
+    val title: String,
+    val header: String,
+    val body: List<String>
+)
--- src/main/kotlin/com/example/googlechartgenerator/ChartController.kt
+++ src/main/kotlin/com/example/googlechartgenerator/ChartController.kt
@@ -2,13 +2,23 @@ package com.example.googlechartgenerator
 
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestBody
 
 
 @Controller
 class ChartController {
-
     @GetMapping("/charts/barchart_material")
-    fun chart(): String {
+    fun chart(@RequestBody requestString: String): String {
+        val convert = requestString.convert()
         return "barchart_material"
     }
 }
+
+private fun String.convert(): BarchartMaterialRequest =
+    split("\n").let {
+        BarchartMaterialRequest(
+            title = it[0],
+            header = it[1],
+            body = it.drop(2)
+        )
+    }
