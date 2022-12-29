4361d8887c3233d8176b03373d135b1aee004fc5
Hyune-c <choi8608@gmail.com>
2022-12-29 18:50:49 +0900

    🔧 init

---
+++ src/main/kotlin/com/example/googlechartgenerator/GoogleChartGeneratorApplication.kt
@@ -0,0 +1,11 @@
+package com.example.googlechartgenerator
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GoogleChartGeneratorApplication
+
+fun main(args: Array<String>) {
+    runApplication<GoogleChartGeneratorApplication>(*args)
+}
